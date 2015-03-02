40108203f6c7ee1e9a24d1042eb88d1a4d2c39f0
Lennon Jesus <lennon.jesus@gmail.com>
2015-03-02 16:22:49 -0300

    habilitando configuracao  mongodb+mongoose

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require('http');
 
 var app = require('./config/express')();
 
+require('./config/database')('mongodb://localhost/mymeanbootstrapprojectdatabasebigname');
+
 http.createServer(app)
 	.listen(app.get('port'), function() {
 		console.log('My MEAN Bootstrap Project iniciado com sucesso!');
